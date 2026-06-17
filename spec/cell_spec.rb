818520f6a4663a5fe761d5fe4e5147fb43a806c5
Katsudon27 <katsudon1127@gmail.com>
2026-06-17 12:03:05 +1000

    Add unit test for cell class
    Added unit test for #add_piece

---
+++ spec/cell_spec.rb
@@ -0,0 +1,22 @@
+require_relative "../lib/cell"
+require_relative "../lib/player"
+require "colorize"
+
+describe Cell do
+  subject(:cell) { described_class.new }
+
+  describe "#add_piece" do
+    context "when the player's colour is red" do
+      let(:player1) { instance_double(Player) }
+
+      before do
+        allow(player1).to receive_messages(symbol: "\u25CF", colour: :red)
+      end
+
+      it "updates its symbol to be same as the player" do
+        cell.add_piece(player1)
+        expect(cell.symbol).to eq("\u25CF".colorize(:red))
+      end
+    end
+  end
+end
