9a094bbb05aa4b46f12ca1de577e72855c3feded
Katsudon27 <katsudon1127@gmail.com>
2026-06-18 11:18:51 +1000

    Add unit test for #player_win?

--- spec/game_controller_spec.rb
+++ spec/game_controller_spec.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/game_controller"
 require_relative "../lib/game_board"
 require_relative "../lib/player"
+require_relative "../lib/cell"
 
 describe GameController do
   subject(:game_controller) { described_class.new(first_player, second_player, game_board) }
@@ -8,6 +9,8 @@ describe GameController do
   let(:first_player) { instance_double(Player) }
   let(:second_player) { instance_double(Player) }
   let(:game_board) { instance_double(GameBoard) }
+  let(:cell) { instance_double(Cell) }
+  let(:created_board) { Array.new(6) { Array.new(7) { cell } } }
 
   describe "#switch_turn" do
     context "when the current player is player 1" do
@@ -26,6 +29,77 @@ describe GameController do
     it "sends #cells_empty? to game board" do
       game_controller.draw_game?
       expect(game_board).to have_received(:cells_empty?)
+    end
+  end
+
+  describe "#player_win?" do
+    before do
+      allow(first_player).to receive(:symbol).and_return("\u25CF".colorize(:red))
+      allow(game_board).to receive(:board).and_return(created_board)
+    end
+
+    context "when a player places four pieces horizontally" do
+      before do
+        allow(created_board[5][0]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[5][1]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[5][2]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[5][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+      end
+
+      it "returns true" do
+        expect(game_controller).to be_player_win
+      end
+    end
+
+    context "when a player places four pieces vertically" do
+      before do
+        allow(created_board[1][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[2][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[3][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[4][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+      end
+
+      it "returns true" do
+        expect(game_controller).to be_player_win
+      end
+    end
+
+    context "when a player places four pieces in an ascending diagonal" do
+      before do
+        allow(created_board[3][0]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[2][1]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[1][2]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[0][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+      end
+
+      it "returns true" do
+        expect(game_controller).to be_player_win
+      end
+    end
+
+    context "when a player places four pieces in a descending diagonal" do
+      before do
+        allow(created_board[0][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[1][4]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[2][5]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[3][6]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+      end
+
+      it "returns true" do
+        expect(game_controller).to be_player_win
+      end
+    end
+
+    context "when a player has not yet won" do
+      before do
+        allow(created_board[0][3]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[1][4]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+        allow(created_board[2][5]).to receive(:symbol).and_return("\u25CF".colorize(:red))
+      end
+
+      it "returns false" do
+        expect(game_controller).not_to be_player_win
+      end
     end
   end
 end
